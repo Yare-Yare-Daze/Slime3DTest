2846cca72e8baee8a9b4590bac256469f67c7067
Mikhail <mikhail.eremeev.work@gmail.com>
2023-03-31 17:24:55 +0300

    First commit

---
+++ Assets/_Scripts/EntityHealth.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+public class EntityHealth : MonoBehaviour
+{
+    [SerializeField] private float startHealth = 100;
+    public Action<float> OnHealthChanged;
+    
+    private float _health;
+    private float _maxHealth;
+
+    public float Health
+    {
+        get
+        {
+            return _health;
+        }
+
+        set
+        {
+            _health = Mathf.Clamp(value, 0, _maxHealth);
+            OnHealthChanged?.Invoke(_health);
+        }
+    }
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        _maxHealth = startHealth;
+        _health = _maxHealth;
+    }
+}
